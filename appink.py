58c921da4116248e8f6c01a447db4e04dad14570
coco <mrkokisounds@gmail.com>
2022-08-31 14:30:46 +0900

    rebase

--- appink.py
+++ appink.py
@@ -1,9 +1,12 @@
-import cv2
-import sys
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import numpy as np
+import cv2
+import os
+from datetime import datetime as dt
+import sys
 args = sys.argv
 
-def colorInk(src, ch1Lower, ch1Upper, ch2Lower, ch2Upper, ch3Lower, ch3Upper):
+def colorInk(src, dst,ch1Lower, ch1Upper,ch2Lower, ch2Upper,ch3Lower, ch3Upper):
     src = cv2.cvtColor(src,cv2.COLOR_BGR2HSV)
     lower = [0,0,0]
     upper = [0,0,0]
@@ -47,7 +50,18 @@ def colorInk(src, ch1Lower, ch1Upper, ch2Lower, ch2Upper, ch3Lower, ch3Upper):
     src = cv2.cvtColor(src,cv2.COLOR_HSV2BGR)
     return src
 
-def colorExtraction1(src,
+def imageclear():
+    cv2.imwrite("./templates/IMG/AKAZE.jpg", 0)
+    cv2.imwrite("./templates/IMG/FakeAKAZE.jpg", 0)
+    cv2.imwrite("./templates/IMG/INPUT.jpg", 0)
+    cv2.imwrite("./templates/IMG/Cyan.jpg", 0)
+    cv2.imwrite("./templates/IMG/Magenda.jpg", 0)
+    cv2.imwrite("./templates/IMG/Yellow.jpg", 0)
+    cv2.imwrite("./templates/IMG/procCyan.jpg", 0)
+    cv2.imwrite("./templates/IMG/procMagenda.jpg", 0)
+    cv2.imwrite("./templates/IMG/procYellow.jpg", 0)
+
+def colorExtraction1(src, dst,
 					 ch1Lower, ch1Upper,
 					 ch2Lower, ch2Upper,
 					 ch3Lower, ch3Upper):
@@ -93,17 +107,21 @@ def colorExtraction1(src,
                     tmp[y,x]= 0
 
     return TEKIOU/bkazu
+def register(ID,img):
+    x = int(img.shape[0]*(1280/img.shape[1]))
+    img = cv2.resize(img, dsize=(1280,x))
+    cv2.imwrite("./templates/RegistIMG/" +str(ID)  +".jpg", img)
 
-def main(args):
-    
+def main(ID,img):
+    x = int(img.shape[0]*(1280/img.shape[1]))
+    img = cv2.resize(img, dsize=(1280,x))
     "1.原画像の入力"
     print("入力中")
-    src = cv2.imread(args[1])
-    src = cv2.imread(args[1])
+    cv2.imwrite("./templates/IMG/INPUT.jpg", img)
+    src = img
+    src = img
     src_img_orig = src
-    x = int(src.shape[0]*(1280/src.shape[1]))
-    src = cv2.resize(src, dsize=(1280,x))
-    print("画像前処理アルゴリズム: " ,args[1])
+    print("The Ink Process: " )
     alpha = 1.0
     size = src.shape
     
@@ -111,18 +129,6 @@ def main(args):
     print("線形変換")
     Max = [0,0,0]
     Min = [255,255,255]
-    for y in range(0,size[0]):
-        for x in range(0,size[1]):
-            for c in range(0,size[2]):
-                if Max[c] < src[y,x][c]:
-                    Max[c] = src[y,x][c]
-                if Min[c] > src[y,x][c]:
-                    Min[c] = src[y,x][c]
-
-    for y in range(0,size[0]):
-        for x in range(0,size[1]):
-            for c in range(0,size[2]):
-                src[y,x][c] = (255 / (Max[c] - Min[c])) * src[y,x][c]
     
     "3.コントラスト調整②-積和演算"
     print("積和演算")
@@ -131,32 +137,16 @@ def main(args):
     CC = [0,0,0]
     beta = [0,0,0]
     new2_img = np.zeros([size[0], size[1]])
-    for y in range(0,size[0]):
-        for x in range(0,size[1]):
-            for c in range(0,size[2]):
-                AVR[c] = AVR[c] + src[y,x][c]
-                CC[c] = CC[c] + 1
-                
-    for i in range(0,3):
-        beta[i] = AVR[i]/CC[i];		    
-        
-    
-    for y in range(0,size[0]):
-        for x in range(0,size[1]):
-            for c in range(0,size[2]):
-                src[y,x][c] = (alpha * (src[y,x][c]-beta[c])+beta[c])
                 
     "4.画像の先鋭化フィルター"
     print("先鋭中")
     k = 1.0
     sharpningKernel8 = np.array([[k, k, k],[k, 1 + (8*k*-1), k],[k, k, k]])
-    dst = cv2.filter2D(src, -1,sharpningKernel8)
 
     "5.画像の彩度の算出(白抜き画像の生成)"
     print("白抜き中")
     Hue = 0.0
     white = np.zeros([size[0], size[1]])
-    Hue = colorExtraction1(src,0,255,  0,120,  140,255)
 
     "6.インク抽出処理とビット反転"
     print("インク抽出")
@@ -167,49 +157,40 @@ def main(args):
     x_Cyan = Hue
     y_Cyan = (-1.043 * x_Cyan) + 186.44
     x_MY = Hue
-    y_Cyan = (1.429 * x_MY) + 45.71
+    y_Cyan = (1.429 * x_MY) + 25.71
     img_Cyan = np.zeros([size[0], size[1],size[2]])
-    img_Cyan = colorInk(src,90, 150 ,70,255, 180, 255)
+    img_Cyan = colorInk(src, img_Cyan,90, 150 ,70,255, 180, 255)
     img_Magenda = np.zeros([size[0], size[1],size[2]])
-    img_Magenda = colorInk(src,150, 255 ,60,255, 180, 255)
+    img_Magenda = colorInk(src, img_Magenda,150, 255 ,60,255, 180, 255)
     img_Yellow = np.zeros([size[0], size[1],size[2]])
-    img_Yellow = colorInk(src,15,65,60,255,180,255)
-    img_White = np.zeros([size[0], size[1],size[2]])
-    img_White = colorInk(src,0, 255 ,0,60, 200, 255)
+    img_Yellow = colorInk(src, img_Yellow,15, 65 ,60,255, 180, 255)
 
     "7.グレースケール変換"
     print("グレースケール")
     img_Cyan = cv2.cvtColor(img_Cyan,cv2.COLOR_BGR2GRAY)
     img_Magenda = cv2.cvtColor(img_Magenda,cv2.COLOR_BGR2GRAY)
     img_Yellow = cv2.cvtColor(img_Yellow,cv2.COLOR_BGR2GRAY)
-    img_White = cv2.cvtColor(img_White,cv2.COLOR_BGR2GRAY)
 
     "8.適応2値化"
     print("適応")
     img_Cyan = cv2.adaptiveThreshold(img_Cyan,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,101,0)
     img_Magenda = cv2.adaptiveThreshold(img_Magenda,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,101,0)
     img_Yellow = cv2.adaptiveThreshold(img_Yellow,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,101,0)
-    img_White = cv2.adaptiveThreshold(img_White,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,101,0)
 
     "9.2値化"
     print("通常")
     retC, img_Cyan = cv2.threshold(img_Cyan,127,255,cv2.THRESH_BINARY)
     retM, img_Magenda = cv2.threshold(img_Magenda,127,255,cv2.THRESH_BINARY)
     retY, img_Yellow = cv2.threshold(img_Yellow,127,255,cv2.THRESH_BINARY)
-    retW, img_White = cv2.threshold(img_White,127,255,cv2.THRESH_BINARY)
     
     "10.出力画像の保存"
     print("保存")
     out = [0,0,0,0]
-    out[0] = "Cyan"  + args[2]
-    out[1] = "Magenda"  + args[2]
-    out[2] = "Yellow"  + args[2]
-    out[3] = "White"  + args[2]
+    out[0] = "./templates/CyanIMG/" + str(ID) + ".jpg"  
+    out[1] = "./templates/MagendaIMG/" + str(ID) + ".jpg" 
+    out[2] = "./templates/YellowIMG/" + str(ID) + ".jpg" 
     cv2.imwrite(out[0], img_Cyan)
     cv2.imwrite(out[1], img_Magenda)
     cv2.imwrite(out[2], img_Yellow)
-    cv2.imwrite(out[3], img_White)
     
     return None
-
-main(args)
