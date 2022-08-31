cfe38736cbfc57f54ce348fa526700fe8cd4e80e
coco <mrkokisounds@gmail.com>
2022-08-31 16:12:30 +0900

    不要なimport

--- appproc.py
+++ appproc.py
@@ -1,14 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import numpy as np
 import cv2
-import os
-from datetime import datetime as dt
 import sys
 import DBDB
-from mysql import MySQL, MySQLConfig
-from repository.mysql.qr_code import QRCode as QRCodeRepo
-from service.qr_code import QRCode as QRCodeSvc
-from llah.descriptor_extractor import DescriptorExtractor
 from llah.keypoint import Keypoint
 
 args = sys.argv
